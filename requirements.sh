1336c2044d3f30a3e9b6e3a9f2e953da10691716
Ivan Yastrebov <easy-quest@mail.ru>
2021-10-11 21:14:52 +0300

    New updates for Heroku deployment

--- requirements.sh
+++ requirements.sh
@@ -1,18 +1,15 @@
-#!/usr/bin/bash
-
-pipenv install \
-    Babel \
-    Flask-Babel \
-    Flask-Login \
-    Flask-Principal \
-    backports_abc \
-    Flask \
-    iso-639 \
-    PyPDF3 \
-    pytz \
-    requests \
-    SQLAlchemy \
-    tornado \
-    Wand \
-    unidecode \
-    lxml 
+Babel
+Flask-Babel
+Flask-Login 
+Flask-Principal 
+backports_abc 
+Flask 
+iso-639 
+PyPDF3 
+pytz 
+requests 
+SQLAlchemy 
+tornado 
+Wand 
+unidecode 
+lxml 
